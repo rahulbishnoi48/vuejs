ab51e27cdcd98ae355d4c9c809f45c57e77f2bf9
rahul <rbishnoi@ex2india.com>
2021-07-12 09:01:27 +0530

    initial layout of app

---
+++ user-profile/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import profile from './components/profile.vue'
+import photos from './components/photos.vue'
+import posts from './components/posts.vue'
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
